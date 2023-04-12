0bd0c6204f86d28310c501e9c86806aafad571e7
Gustavo Henrique <84508964+guustavo07@users.noreply.github.com>
2023-04-12 09:38:09 -0300

    camadas

---
+++ APIChurrascaria/APIChurrascaria/Program.cs
@@ -0,0 +1,29 @@
+//usavel
+using APIChurrascaria.Controllers;
+using APIChurrascaria.Infra;
+using APIChurrascaria.Servicos.JWT;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors();
+builder.Services.AddControllers();
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+builder.Services.AddVersioning();
+builder.Services.AddSwagger();
+builder.Services.AddAuthentication(builder.Configuration);
+builder.Services.RegisterServices(builder.Configuration);
+
+var app = builder.Build();
+
+app.UseSwaggerUI();
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseCors(builder => builder
+    .SetIsOriginAllowed(orign => true)
+    .AllowAnyMethod()
+    .AllowAnyHeader()
+    .AllowCredentials());
+app.MapControllers();
+
+app.Run();
